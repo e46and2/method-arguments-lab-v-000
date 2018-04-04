f2f8c6cf6ebb46453820699d7247aa9842d66955
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-04-04 23:53:16 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,5 +7,5 @@ end
 #introduction_with_language
 
 def introduction_with_anguage(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
